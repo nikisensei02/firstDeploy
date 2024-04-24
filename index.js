6ae4c41201461989c36533dae0349be80d6a0a4c
nikisensei02 <148560578+nikisensei02@users.noreply.github.com>
2024-04-24 13:54:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import connection from './conn/conn.js';
 import auth from './routes/auth.js';
 import list from './routes/list.js';
 import cors from 'cors';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 1000;
@@ -16,16 +19,18 @@ connection;
 app.use(express.json());
 app.use(cors());
 
-
-
 // API routes
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Serve React app for all other routes
+// For any other route, serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-    
 // Start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
